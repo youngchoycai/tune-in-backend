00705fc36bed4720df7502383f5f40ccdcf61fab
Young Cai <youngcai345@berkeley.edu>
2020-10-15 23:52:00 -0700

    new auth

--- api.py
+++ api.py
@@ -103,15 +103,14 @@ def api_callback():
     return redirect("index")
     """
     sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id = spot_client_id, client_secret = spot_client_secret,redirect_uri = spot_client_redirect, scope=scope)
-    flasksession.clear()
-    #code = request.args.get('code')
-    #token_info = sp_oauth.get_access_token(code)
+    
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
 
     # Saving the access token along with all other token related info
-    #flasksession["token_info"] = token_info
+    
 
-
-    return "owo"
+    return token_info + " " + code
     
 @app.route('/api/create', methods = ['GET'])
 def create_party():
